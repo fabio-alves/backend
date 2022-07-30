5013f2eac69d911a0acd1c3cfeb5795ac77d608d
fabio alves <fabio.i9@live.com>
2022-07-30 18:42:04 -0300

    refactor: create a interface sutType for makeSut

--- src/data/useCases/addUser/dbAddUser.spec.ts
+++ src/data/useCases/addUser/dbAddUser.spec.ts
@@ -1,14 +1,28 @@
+import { Encrypter } from '../../protocols/encrypter';
 import { DbAddUser } from './dbAddUser';
+
+interface SutTypes {
+  sut: DbAddUser,
+  encrypterStub: Encrypter,
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypter(value: string): Promise<string> {
+      return new Promise(resolve => resolve('hasehdPassword'));
+    }
+  }
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddUser(encrypterStub);
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
 describe('DbAddUserUseCase', () => {
   test('should call encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypter(value: string): Promise<string> {
-        return new Promise(resolve => resolve('hasehdPassword'));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddUser(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypter');
     const userData = {
       name: 'validName',
